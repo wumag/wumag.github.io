a9f4a8b83f1a5ef8fde8d0fe4656d078789b891c
wumag <107904188+wumag@users.noreply.github.com>
2023-01-03 14:30:34 -0800

    Add files via upload

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,113 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import LineGradient from "../components/LineGradient";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-2/5 z-10 mt-16 md:mt-6 flex justify-center md:order-2">
+        {isAboveLarge ? (
+          <div
+          // className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+          // before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="assets/profile.png"
+            />
+          </div>
+        ) : (
+          <img
+            alt="profile"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src="assets/profile.png"
+          />
+        )}
+      </div>
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-5xl font-playfair z-10 text-center md:text-start mb-5">
+            Hello, I'm {""}
+            <span
+              className="xs:relative xs:font-bold z-20"
+            >
+              Maggie
+            </span>
+          </p>
+          <LineGradient width="w-6/7" />
+          <i className="mt-10 mb-7 font-playfair text-sm text-center md:text-start">
+            post-baccalaureate computer science student
+          </i>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-10 mb-7 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-black rounded-sm py-3 px-6 font-semibold
+              hover:bg-blue hover:text-yellow transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <pre>    </pre>
+          <a
+            className="bg-gradient-rainblue text-black rounded-sm py-3 px-6 font-semibold
+            hover:bg-blue hover:text-yellow transition duration-500"
+            href="https://drive.google.com/file/d/1T1opk9b1yZctFqy-cV5EczhAar9hU52o/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-10 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
