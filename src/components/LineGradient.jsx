8f67022a3118f876c39ab8dbac3c825e97b7a2dc
wumag <wumag@oregonstate.edu>
2023-02-07 17:42:00 -0800

    Updates

--- src/components/LineGradient.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component }  from 'react';
-
-const LineGradient = ({ width = "w-full" }) => {
-  return <div className={`h-0.5 ${width} bg-gradient-rainblue`} />;
-};
-
-export default LineGradient;
